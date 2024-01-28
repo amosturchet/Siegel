e54a7d76c4a674b2e49a0dfd53b09f5bf07adfe1
fbarroero <fbarroero@gmail.com>
2024-01-28 13:48:22 +0100

    Scheletro di dimostrazione

--- Siegel/siegel_lemma.lean
+++ Siegel/siegel_lemma.lean
@@ -2,19 +2,14 @@ import Mathlib
 
 --statement of Siegel's Lemma, version 1 for the the integers
 open Matrix
-
 open BigOperators
 
 attribute [local instance] Matrix.linftyOpNormedAddCommGroup
 
-variable (m n : ℕ) (A : Matrix (Fin m) (Fin n) ℤ)
+variable (m n : ℕ) (A : Matrix (Fin m) (Fin n) ℤ) (v : Fin n → ℤ )
 
-#check ‖A‖
+--#check ‖A‖
 
--- maybe n = m + k  0 < k
-theorem siegelsLemma  (hn: m < n) (hA : A ≠ 0 ) :
-   ∃ (t: Fin n → ℤ), t ≠ 0 ∧ A.mulVec t = 0 ∧ ‖t‖^(n-m) ≤ (n*‖A‖)^m    := by
-  sorry
 
 
 
@@ -23,7 +18,7 @@ theorem siegelsLemma  (hn: m < n) (hA : A ≠ 0 ) :
 
 variable (Lp Lm : Fin n → ℤ)
 
-#check (Finset.Icc Lm Lp).card
+#check (Finset.Icc Lm Lp)
 
 -- cardinality of integer points in a product of intervals
 -- uses Icc
@@ -37,3 +32,54 @@ lemma aux1 (h : Lm ≤ Lp) : (Finset.Icc Lm Lp).card =
  ring
 
 --(Lp  i + Lm i + 1) := by
+
+
+--Pigeonhole Finset.exists_ne_map_eq_of_card_lt_of_maps_to
+
+--#check Σ j :Fin n , A (0 : Fin m) ( 0 : Fin
+
+--variables (i : Fin m ) (j : Fin m)
+--#check (∏  j : Fin n , (A i j))
+-- maybe n = m + k  0 < k
+theorem siegelsLemma  (hn: m < n) (hm: 0 < m) (hA : A ≠ 0 ) :
+      ∃ (t: Fin n → ℤ), t ≠ 0 ∧ A.mulVec t = 0 ∧ ‖t‖^(n-m) ≤ (n*‖A‖)^m    := by
+   let B:= Nat.floor ((n*‖A‖)^(m/(n-m)))
+   let B':= fun j : Fin n => (B: ℤ )
+   let T:= Finset.Icc 0 B'
+   have hcardT : T.card=(B+1)^n := by
+      simp
+      rw [Pi.card_Icc 0 B']
+      simp
+   let P := fun i : Fin m =>( ∑  j : Fin n , B*( if 0 < (A i j) then A i j else 0))
+   let N := fun i : Fin m =>( ∑  j : Fin n , B*( if (A i j) < 0 then -(A i j) else 0))
+   let S:= Finset.Icc (-N) (P)
+   let C:= Nat.floor ((‖A‖*n*B+1)^m)
+   have hcardS : S.card=C := by
+      sorry
+   have hcardineq : S.card<T.card := by sorry
+      -- rw [hcardT, hcardS]
+      -- have haux : (C : ℝ)  < (B + 1) ^ n := by sorry
+      -- norm_num  at haux
+      -- norm_num
+      -- norm_cast
+      -- qify
+
+   let f:= fun v : (Fin n → ℤ ) => A.mulVec v
+   have him : ∀ v ∈  T, (f v) ∈  S := sorry
+   rcases Finset.exists_ne_map_eq_of_card_lt_of_maps_to hcardineq him with ⟨ x, hxT,y, hyT ,hneq, hfeq⟩
+   use x-y
+   constructor
+   · exact sub_ne_zero.mpr hneq
+   constructor
+   ·  simp at hfeq
+      sorry
+
+   sorry
+
+
+
+
+   --have him : ∀ v : Fin n → ℤ , ‖ A.mulVec v‖≤ (‖A‖ * n * B +1) := sorry
+
+
+   --rcases Finset.exists_ne_map_eq_of_card_lt_of_maps_to

beef339d3c825cc64a8ff20101428a833a1215dd
fbarroero <fbarroero@gmail.com>
2024-04-03 16:06:20 +0200

    Try generalize Riccardo's Lemma

--- Siegel/Riccardo_lemma.lean
+++ Siegel/Riccardo_lemma.lean
@@ -14,3 +14,13 @@ lemma bar : ∃ (k : ℕ), ‖M‖ = k := by
   simp_rw [norm_def, Pi.norm_def, Pi.nnnorm_def, ← NNReal.coe_nat_cast, foo]
   congr; ext; congr; ext
   simp [Nat.cast_natAbs, Int.norm_eq_abs]
+
+section Mathlib
+open Algebra Order Monoid WithZero
+
+variable {M : Type*} [LinearOrderedCommMonoid M] {n m : ℕ} (A : Matrix (Fin m) (Fin n) M)
+
+/- lemma foo' {A : Type*} [Fintype A] (f : A → M) : (sup univ f) = sup univ fun b ↦ (f b : NNReal) :=
+  comp_sup_eq_sup_comp_of_is_total _ Nat.mono_cast (by simp) -/
+
+end Mathlib
